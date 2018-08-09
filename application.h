14f1dca79ab6f1d8f24e9b1ff1b543caaccc7798
ignacioVidaurreta <ignacioVidaurreta@users.noreply.github.com>
2018-08-09 12:29:44 -0300

    oops

--- application.h
+++ application.h
@@ -2,9 +2,8 @@
 #define APPLICATION_H
 #define SLAVE_DIR "./slave"
 #define NUM_WORKERS 10
-  void forker2(char * files[]);
-  int everyProcessEnded(int childPid[NUM_WORKERS]);
-
+  void forker(char * files[]);
+ 
 
 
 #endif
