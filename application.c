c4de3c5fd6fc9c8fd01ad400ba00dd3bf7d83c4a
Thomas Beck <thomas.beck@grenoble-inp.org>
2018-08-13 15:03:14 -0300

    One pipe for all childs, waitpid in parent and close pipe to end child process.

--- application.c
+++ application.c
@@ -6,31 +6,38 @@
 #include <semaphore.h>
 
 #define SLAVE_DIR "./slave"
-#define NUM_WORKERS 1
+#define NUM_WORKERS 2 
 
 
 int main (int argc, char * argv[]){
 
-    int fds[NUM_WORKERS][2];
+    int fd[2];
+    pipe(fd);
 
     int num_files = argc - 1; 
     int child_pid[NUM_WORKERS];
     int num_init = num_files/(NUM_WORKERS*4) + 1;
     for (int i = 0; i < NUM_WORKERS; i++) {
-        pipe(fds[i]);
         child_pid[i] = fork();
         if (!child_pid[i]) {
-            close(fds[i][1]);
-            dup2(fds[i][0], 0);
+            close(fd[1]);
+            dup2(fd[0], 0);
 
             execl(SLAVE_DIR, "slave", NULL);
         } else {
             for (int j = 0; j < num_init; j++) {
-                dprintf(fds[i][1], "%s\n", argv[i*num_init + j + 1]);
+                dprintf(fd[1], "%s\n", argv[i*num_init + j + 1]);
                 num_files--;
             }
         }
     }
+    
+    close(fd[0]);
+    close(fd[1]);
+    for (int i = 0; i < NUM_WORKERS; i++) {
+        waitpid(child_pid[i], NULL, 0);
+    }
+
     return 0;
 }
 
