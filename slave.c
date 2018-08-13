513d5afcc5dee847f0ebfac0331bdc1487931000
Thomas Beck <thomas.beck@grenoble-inp.org>
2018-08-13 18:28:37 -0300

    minor changes

--- slave.c
+++ slave.c
@@ -11,13 +11,14 @@
 #include "include/slave.h"
 
 int main(int argc, char * argv[]){
+    int ppid = getppid();
     size_t size;
     char *line = NULL;
     int num_char = getline(&line, &size, stdin);
     while (num_char != -1) {
         line[num_char - 1] = '\0';
         md5_file(line);
-        kill(getppid(), SIGUSR1);
+        kill(ppid, SIGUSR1);
         num_char = getline(&line, &size, stdin);
     }
     return 0;
@@ -28,6 +29,7 @@ void md5_file(char *file) {
     if (pid) {
         waitpid(pid, NULL, 0);
     } else {
+        // For now, output of md5sum is stdout, but need to be connected to viewer
          execl("/usr/bin/md5sum", "md5sum", file, NULL);
     }
 }
