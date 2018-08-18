58108e7f65ed05bce659107c4e511fc350627dc6
Naco <igvidaurreta@gmail.com>
2018-08-18 12:57:42 -0300

    It no longer stays idle at the end & modified #defines to go to the .h

--- include/application.h
+++ include/application.h
@@ -2,12 +2,13 @@
 #include <semaphore.h>
 #define APPLICATION_H
 #define SLAVE_DIR "./slave"
-#define NUM_WORKERS 1
+#define NUM_WORKERS 2
+#define BASE_SIZE 1000
 
 sem_t *sem;
 
 void forker(char * files[]);
- 
+
 
 
 #endif
